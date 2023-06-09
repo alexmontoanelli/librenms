4d10e64246faf5098b19f5a1ff730634771ca175
Alex Montoanelli <alexmontoanelli@gmail.com>
2023-06-09 10:35:39 -0300

    billing

--- resources/views/w8/billing-email.blade.php
+++ resources/views/w8/billing-email.blade.php
@@ -13,6 +13,7 @@
 
 - Contratado: {{str_pad(LibreNMS\Util\Number::formatSi($history[0]['bill_allowed'],2,3,'bps'), 10, ' ', STR_PAD_LEFT)}}
 - Excedente: {{str_pad(LibreNMS\Util\Number::formatSi($history[0]['bill_overuse'],2,3,'bps'), 10, ' ', STR_PAD_LEFT)}}
+- Aferido {{str_pad(LibreNMS\Util\Number::formatSi($history[0]['rate_95th'],2,3,'bps'), 10, ' ', STR_PAD_LEFT)}}
 @if (preg_match('/cdr/', $bill['bill_type']))
 - Método: Percentil {{$percentil}}%
 @endif
@@ -20,16 +21,24 @@
 # Histórico
 
 ```
-Referência    Tipo    Permitido          Excedente
+Referência | Tipo | Contratado | Aferido | Excedente
 
 @php
-array_shift($history);
+//array_shift($history);
+
 @endphp
 @foreach($history as $h)
+@php
+$metodo = $h['percentil'];
+if ($metodo == null) $metodo = '95%';
+if (!str_ends_with($metodo, '%')){
+    $metodo .= '%';
+}
+@endphp
 @if ($h['bill_overuse'] > 0)
-{{substr($h['bill_datefrom'],0,7)}}    {{$h['percentil'] ?? '95%'}}    {{str_pad(LibreNMS\Util\Number::formatSi($h['bill_allowed'],2,3,'bps'), 10, ' ', STR_PAD_LEFT)}}          {{str_pad(LibreNMS\Util\Number::formatSi($h['bill_overuse'],2,3,'bps'), 10,' ', STR_PAD_LEFT)}}
+{{substr($h['bill_datefrom'],0,7)}} | {{$metodo}} | {{str_pad(LibreNMS\Util\Number::formatSi($h['bill_allowed'],2,3,'bps'), 10, ' ', STR_PAD_LEFT)}} | {{str_pad(LibreNMS\Util\Number::formatSi($h['rate_95th'],2,3,'bps'), 10,' ', STR_PAD_LEFT)}} | {{str_pad(LibreNMS\Util\Number::formatSi($h['bill_overuse'],2,3,'bps'), 15,' ', STR_PAD_LEFT)}}
 @else
-{{substr($h['bill_datefrom'],0,7)}}    {{$h['percentil'] ?? '95%'}}    {{str_pad(LibreNMS\Util\Number::formatSi($h['bill_allowed'],2,3,'bps'), 10, ' ', STR_PAD_LEFT)}}          -
+{{substr($h['bill_datefrom'],0,7)}} | {{$metodo}} | {{str_pad(LibreNMS\Util\Number::formatSi($h['bill_allowed'],2,3,'bps'), 10, ' ', STR_PAD_LEFT)}} | {{str_pad(LibreNMS\Util\Number::formatSi($h['rate_95th'],2,3,'bps'), 10,' ', STR_PAD_LEFT)}} | {{str_pad('-', 15,' ', STR_PAD_LEFT)}}
 @endif
 @endforeach
 ````
